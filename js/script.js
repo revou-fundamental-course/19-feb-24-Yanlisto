cb7127b503d8c9cb3e3030cc81fea65ad95da8a2
Yanlisto <yanlisto.yanlisto@gmail.com>
2024-02-29 22:27:50 +0700

    first push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let name = prompt("What is Your Name");
+// if (show == null || show == ""){
+//     txt = "No Name";
+// } else {
+//     txt = "Hello, Welcome " + show;
+// }
+// alert(txt);
+document.getElementById("name").innerHTML = name
+
+console.log("name");
